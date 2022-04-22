d0aa3101135d37fa71bea38fc8f7606431cfe4d2
justin <jtr342@gmail.com>
2022-04-21 20:54:58 -0700

    filtering out columns from request

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from cxn import Connection
-import os
 from dotenv import load_dotenv
+import os
+import json
 
 load_dotenv()
 url = "http://api.travelpayouts.com/v2/prices/nearest-places-matrix?currency=usd&origin=SAN&destination=SFO&show_to_affiliates=true&token="
 token = os.getenv('api-key')
 connection = Connection(token, os.getenv('marker'))
 response = connection.get(url+token)
-with open ("response.json", "w") as f:
-    f.write(response.text)
+jsonObject = json.loads(response.text)
+prices = jsonObject["prices"]
+filtered_columns = [{k: v for k, v in d.items() if k == 'price' or k=='destination' or k == 'origin' or k == 'depart_date'} for d in prices]
+print(filtered_columns)
