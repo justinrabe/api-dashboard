1e4d9b28aa5c2d19c4c1f4ca195af6fb40a30301
justin <jtr342@gmail.com>
2022-04-22 21:50:39 -0700

    laying ground work for insertion into DB

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from cxn import Connection
 from dotenv import load_dotenv
 import os
 import json
+import mysql.connector
 
 load_dotenv()
 url = "http://api.travelpayouts.com/v2/prices/nearest-places-matrix?currency=usd&origin=SAN&destination=SFO&show_to_affiliates=true&token="
@@ -10,5 +11,6 @@ connection = Connection(token, os.getenv('marker'))
 response = connection.get(url+token)
 jsonObject = json.loads(response.text)
 prices = jsonObject["prices"]
-filtered_columns = [{k: v for k, v in d.items() if k == 'price' or k=='destination' or k == 'origin' or k == 'depart_date'} for d in prices]
-print(filtered_columns)
+filtered_columns = [{k: v for k, v in d.items() if k == 'price' or k == 'depart_date'} for d in prices]
+print(type(filtered_columns[0]["depart_date"]))
+
