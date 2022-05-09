19224abb83c0d11bdbc975afe558b5c8b7e5edcf
justin <jtr342@gmail.com>
2022-05-08 22:58:32 -0700

    API no longer returning data, need to debug

--- test.py
+++ test.py
@@ -12,8 +12,9 @@ def api_to_db():
     connection = Connection(token, os.getenv('marker'))
     response = connection.get(url+token)
     prices = connection.to_prices(response)
+    print(prices)
     filtered_columns = [{k: v for k, v in d.items() if k == 'price' or k == 'depart_date' or k == 'destination' or k == 'origin'} for d in prices]
     print(filtered_columns)
-    python_mysql_dbconfig.insert(filtered_columns)
+    #python_mysql_dbconfig.insert(filtered_columns)
 
 api_to_db()
